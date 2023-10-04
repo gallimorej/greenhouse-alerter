1f33d6277770d7fcc83063a3e7992b7e69bf5729
Jeff Gallimore <jeff.gallimore@excella.com>
2023-10-04 16:25:37 -0700

    Add failing tests

--- src/tests/test_app.py
+++ src/tests/test_app.py
@@ -54,5 +54,59 @@ class TestFlaskRoutes(TestCase):
         self.assertIn(f"Checked greenhouse temperature at {current_time}", str(response.data))
         mock_stop_fans.assert_called_once()
 
+    @patch('app.get_secret')  # Replace the path with your actual import
+    @patch('app.PySensorPush')  # Replace the path with your actual import
+    def test_test_sensorpush_connection_no_secret(self, MockPySensorPush, MockGetSecret):
+        MockGetSecret.return_value = None
+
+        with patch.dict('os.environ', {'TEMPERATURE_HIGH_TRIGGER': '75', 'GREENHOUSE_SENSOR_ID': 'some_id', 'SAMPLE_LIMIT': '20'}):
+            response = self.client.get('/test-sensorpush-connection')
+            self.assertEqual(response.status_code, 500)
+            self.assertIn(b"Failed to retrieve secret from Secret Manager", response.data)
+
+    @patch('app.get_secret')  # Replace the path with your actual import
+    @patch('app.PySensorPush')  # Replace the path with your actual import
+    def test_test_sensorpush_connection_no_sensor_id(self, MockPySensorPush, MockGetSecret):
+        MockGetSecret.return_value = "some_value"
+        mock_sensor = Mock()
+        mock_sensor.samples.return_value = {'sensors': {'some_other_id': [{'temperature': '70.0'}]}}
+        MockPySensorPush.return_value = mock_sensor
+
+        with patch.dict('os.environ', {'TEMPERATURE_HIGH_TRIGGER': '75', 'GREENHOUSE_SENSOR_ID': 'some_id', 'SAMPLE_LIMIT': '20'}):
+            response = self.client.get('/test-sensorpush-connection')
+            self.assertEqual(response.status_code, 500)
+            self.assertIn(b"No sensor data found for sensor ID some_id", response.data)
+
+    @patch('app.get_secret')  # Replace the path with your actual import
+    @patch('app.IftttWebhook')  # Replace the path with your actual import
+    def test_test_ifttt_connection_no_secret(self, MockIftttWebhook, MockGetSecret):
+        MockGetSecret.return_value = None
+
+        response = self.client.get('/test-ifttt-connection')
+        self.assertEqual(response.status_code, 500)
+        self.assertIn(b"Failed to retrieve secret from Secret Manager", response.data)
+
+    @patch('app.get_secret')  # Replace the path with your actual import
+    @patch('app.IftttWebhook')  # Replace the path with your actual import
+    def test_test_ifttt_connection_no_key(self, MockIftttWebhook, MockGetSecret):
+        MockGetSecret.return_value = "some_value"
+        mock_ifttt = Mock()
+        mock_ifttt.trigger.return_value = False
+        MockIftttWebhook.return_value = mock_ifttt
+
+        response = self.client.get('/test-ifttt-connection')
+        self.assertEqual(response.status_code, 500)
+        self.assertIn(b"Failed to trigger IFTTT webhook", response.data)
+
+    @patch('app.is_greenhouse_too_hot', return_value=False)
+    @patch('app.is_greenhouse_cool_enough', return_value=False)
+    def test_check_temp_not_hot_enough(self, mock_is_cool_enough, mock_is_too_hot):
+        response = self.client.get('/check-greenhouse-temp')
+        current_time = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+        
+        self.assertEqual(response.status_code, 200)
+        self.assertIn(f"Checked greenhouse temperature at {current_time}", str(response.data))
+        self.assertNotIn(b"Starting greenhouse fans", response.data)
+        self.assertNotIn(b"Stopping greenhouse fans", response.data)
 if __name__ == '__main__':
     unittest.main()
